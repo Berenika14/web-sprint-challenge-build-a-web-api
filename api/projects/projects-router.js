da537147ef7aef709855141e35247837a42e1bf1
Berenika Ahmetaj <ahmetajberenika@gmail.com>
2022-02-11 13:08:04 -0500

    Build the api with get method to get all the projects

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,17 @@
 // Write your "projects" router here!
+const express = require("express");
+const Projects = require("./projects-model");
+
+const router = express.Router();
+
+router.get("", (req, res) => {
+  Projects.get()
+    .then((projects) => {
+      res.status(200).json(projects);
+    })
+    .catch(() => {
+      res.status(500).json({ message: "Error retrieving the projects" });
+    });
+});
+
+module.exports = router;
